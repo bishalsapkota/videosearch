db3338c6e94fea81ee48389691a9377649f7ebc3
bishalsapkota <bishalsap2012@gmail.com>
2019-02-18 18:22:40 +0545

    Updates

---
+++ precache-manifest.481d3adb1e2bb79323b8fdacab8faf3f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d6342ef40fb10022c56",
+    "url": "/videosearch/static/css/main.d15738ce.chunk.css"
+  },
+  {
+    "revision": "5d6342ef40fb10022c56",
+    "url": "/videosearch/static/js/main.5d6342ef.chunk.js"
+  },
+  {
+    "revision": "14aed904f203e472bec2",
+    "url": "/videosearch/static/js/runtime~main.14aed904.js"
+  },
+  {
+    "revision": "9d50350833801af6c383",
+    "url": "/videosearch/static/js/2.9d503508.chunk.js"
+  },
+  {
+    "revision": "d4e11d160918178978c89736773b73cb",
+    "url": "/videosearch/index.html"
+  }
+];
